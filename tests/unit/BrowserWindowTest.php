5208a993578e329faa09a7b1f03e473da7ab1dba
Dennis Verspuij <dennisverspuij@users.noreply.github.com>
2017-03-29 15:57:31 +0000

    Added test case for windowing methods manipulating stacks of windows

--- tests/unit/BrowserWindowTest.php
+++ tests/unit/BrowserWindowTest.php
@@ -57,4 +57,75 @@ class BrowserWindowTest extends BrowserCommandsTestCase {
     $this->assertTrue($this->browser->switchToWindow("1"));
     $this->assertEquals(1, $this->browser->windowHandle());
   }
+
+  public function testWindowTree() {
+    // Open a stack of windows like following array of window names by handles:
+    $expectedStack = array(
+      0 =>       array('', 'windowing.html'),
+        1 =>     array('', 'windowing.html'),
+        2 =>     array('second', 'windowing.html'),
+          3 =>   array('', 'windowing.html'),
+          4 =>   array('third', 'windowing.html'),
+            6 => array('', 'windowing.html'),
+      5 =>       array('BASIC_WINDOW', 'basic.html'),
+    );
+    $this->visitUrl($this->getTestPageBaseUrl() . "/static/windowing.html"); // Open 0
+    $element = $this->browser->find('xpath', '//*[@id="to_new"]');
+    $this->browser->click($element['page_id'], $element['ids'][0]);          // Open 1 from 0
+    $element = $this->browser->find('xpath', '//*[@id="to_second"]');
+    $this->browser->click($element['page_id'], $element['ids'][0]);          // Open 2 from 0
+    $this->browser->switchToWindow('2');
+    $element = $this->browser->find('xpath', '//*[@id="to_new"]');
+    $this->browser->click($element['page_id'], $element['ids'][0]);          // Open 3 from 2
+    $element = $this->browser->find('xpath', '//*[@id="to_third"]');
+    $this->browser->click($element['page_id'], $element['ids'][0]);          // Open 4 from 2
+    $this->browser->switchToWindow('0');
+    $element = $this->browser->find('xpath', '//*[@id="to_new_basic"]');
+    $this->browser->click($element['page_id'], $element['ids'][0]);          // Open 5 from 0
+    $this->browser->switchToWindow('4');
+    $element = $this->browser->find('xpath', '//*[@id="to_new"]');
+    $this->browser->click($element['page_id'], $element['ids'][0]);          // Open 6 from 4
+
+    // Validate stack
+    $validateStack = function() use (&$expectedStack) {
+      $currentHandle = $this->browser->windowHandle();
+      $actual = array();
+      foreach($this->browser->windowHandles() as $handle) {
+        $this->browser->switchToWindow($handle);
+        $actual[$handle] = array($this->browser->windowName(), basename($this->browser->currentUrl()));
+      }
+      $this->assertEquals($expectedStack, $actual);  // assertEquals ignores key order
+      $this->browser->switchToWindow($currentHandle);
+    };
+    $validateStack();
+
+    // Open auth_ok.html from 1 into window 'third' (4), then revalidate stack and url of 'third'
+    $this->browser->switchToWindow('1');
+    $element = $this->browser->find('xpath', '//*[@id="to_third_auth_ok"]');
+    $this->browser->click($element['page_id'], $element['ids'][0]);
+    $expectedStack[$this->browser->windowHandle('third')][1] = 'auth_ok.html';
+    $validateStack();
+
+    // Close 3 and revalidate stack
+    $this->browser->closeWindow('3');
+    unset($expectedStack[3]);
+    $validateStack();
+
+    // Close 2, and thereby also 4 and 6, and revalidate stack
+    $this->browser->closeWindow('2');
+    unset($expectedStack[2], $expectedStack[4], $expectedStack[6]);
+    $validateStack();
+
+    // Close all others
+    $this->browser->closeWindow('0');
+    $this->assertEquals(null, $this->browser->windowHandles());
+    $this->assertEquals(null, $this->browser->windowHandle());
+    try {
+      $this->fail($this->browser->windowName());
+    } catch(\Exception $e) {
+      $this->assertInstanceOf('Zumba\GastonJS\Exception\NoSuchWindowError', $e);
+    }
+    $this->browser->reset();
+    $this->assertEquals(0, $this->browser->windowHandle());
+  }
 }
